d75b49d0d92f990aef9332d2dd5c0104a19f64de
arshdeep singh <arshdeep4909@gmail.com>
2022-02-13 20:26:52 -0500

    completed Project.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # calculator-app
+
+getDisplayNumber(number) {
+// function add comma after every three digits in our output
+const floatNumber = parseFloat(number); //converts string to a floating number
+if (isNaN(floatNumber)) return "";
+return floatNumber.toLocaleString("en");
+}
+
+using this function does add comma after every three
+digits however,
+
+1.  we cannot start the number with decimal
+    becase we have isNaN in our if statement, hence it sets it
+    to empty string,
+2.  we cannot type more than three digits after the decimal here
+
+Hence, we need to split the number in two parts
+
+integer - before the decimal place
+decimal - comes after the decimal place
--- app.js
+++ app.js
@@ -65,12 +65,34 @@ class Calculator {
     this.currentOperand = this.currentOperand.toString().slice(0, -1);
   }
 
-  updateDisplay() {
-    this.currentOperandTextElement.innerText = this.currentOperand;
-    if (this.operation != null) {
-      this.previousOperandTextElement.innerText = `${this.previousOperand}${this.operation}`;
+  getDisplayNumber(number) {
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split(".")[0]);
+    const decimalDigits = stringNumber.split(".")[1];
+    let integerDisplay;
+    if (isNaN(integerDigits)) {
+      integerDisplay = "";
     } else {
-      this.previousOperandTextElement.innerText = this.previousOperand;
+      integerDisplay = integerDigits.toLocaleString("en", {
+        maximumFractionDigits: 0,
+      });
+    }
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`;
+    } else {
+      return integerDisplay;
+    }
+  }
+  updateDisplay() {
+    this.currentOperandTextElement.innerText = this.getDisplayNumber(
+      this.currentOperand
+    );
+    if (this.operation != null) {
+      this.previousOperandTextElement.innerText = `${this.getDisplayNumber(
+        this.previousOperand
+      )} ${this.operation}`;
+    } else {
+      this.previousOperandTextElement.innerText = "";
     }
   }
 }
